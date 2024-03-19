3fafd0e889756286c664714a436ee006f064d4b4
Willem Olding <willemolding@gmail.com>
2024-03-19 19:01:49 +1100

    add logic to tally burns

--- tiny-cash/src/write.rs
+++ tiny-cash/src/write.rs
@@ -1,15 +1,33 @@
-use std::{
-    future::Future, pin::Pin, sync::Arc, task::{Context, Poll}
-};
-use futures_util::future::FutureExt;
-use tower::{Service, ServiceExt};
 use chrono::{DateTime, Utc};
+use futures_util::future::FutureExt;
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
+use tower::{Service, ServiceExt};
 
-use zebra_chain::{amount::{Amount, NonNegative}, block::{Block, Header, Height}, fmt::HexDebug, parameters::Network, transaction::LockTime, work::{difficulty::CompactDifficulty, equihash::Solution}};
 use zebra_chain::block;
 use zebra_chain::transaction::Transaction;
 use zebra_chain::transparent;
+use zebra_chain::{
+    amount::{Amount, NonNegative},
+    block::{Block, Header, Height},
+    fmt::HexDebug,
+    parameters::Network,
+    transaction::LockTime,
+    transparent::Script,
+    work::{difficulty::CompactDifficulty, equihash::Solution},
+};
 use zebra_consensus::transaction as tx;
+use zebra_state::FromDisk;
+
+// outputs locked with this script are considered burned and can be released on L1
+// this script pushed false to the stack so funds can never be spent
+fn mt_doom() -> Script {
+    Script::new(&[0])
+}
 
 pub struct TinyCashWriteService<S, V> {
     state_service: S,
@@ -41,9 +59,9 @@ pub enum Request {
 /// The response type for the TinyCash service
 pub struct Response {
     /// Hash of the block that was added by this state transition
-    block_hash: block::Hash,
+    pub block_hash: block::Hash,
     /// The amount of coins that were burned by the transaction (if any) by transferring to the Mt Doom address (0x000...000)
-    burned: Amount<NonNegative>,
+    pub burned: Amount<NonNegative>,
 }
 
 impl<S, V> tower::Service<Request> for TinyCashWriteService<S, V>
@@ -78,7 +96,7 @@ where
         self.state_service.poll_ready(cx)
     }
 
-    fn call(&mut self, req: Request) -> Self::Future {        
+    fn call(&mut self, req: Request) -> Self::Future {
         let mut state_service = self.state_service.clone();
         let mut transaction_verifier = self.tx_verifier_service.clone();
 
@@ -97,7 +115,15 @@ where
                 (vec![Arc::new(coinbase_tx)], burned)
             }
             Request::IncludeTransaction { transaction } => {
-                todo!();
+                let coinbase_tx = empty_coinbase_txn(height); // this is required for block validity
+                let burned = transaction // tally up any burn spends
+                    .outputs()
+                    .iter()
+                    .filter(|output| output.lock_script == mt_doom())
+                    .map(|output| output.value)
+                    .reduce(|total, elem| (total + elem).expect("overflow")) // FIX: exploit here where you could freeze the rollup by burning more than the Amount value can hold
+                    .unwrap_or(Amount::zero());
+                (vec![Arc::new(coinbase_tx), Arc::new(transaction)], burned)
             }
         };
 
@@ -120,7 +146,7 @@ where
         println!("block: {:?}", block);
 
         // the below checks are from the zebra-consensus block verifier
-        // this logic mostly taken from zebra-consensus block verifier 
+        // this logic mostly taken from zebra-consensus block verifier
         // https://github.com/ZcashFoundation/zebra/blob/main/zebra-consensus/src/block.rs
 
         let block_hash = block.hash();
@@ -148,11 +174,7 @@ where
             }
 
             // return the info about the new block
-            Ok(Response {
-                block_hash,
-                burned,
-            })
-
+            Ok(Response { block_hash, burned })
         }
         .boxed()
     }
@@ -184,6 +206,14 @@ fn mint_coinbase_txn(
     }
 }
 
+fn empty_coinbase_txn(height: Height) -> Transaction {
+    mint_coinbase_txn(
+        Amount::zero(),
+        &transparent::Address::from_bytes([0; 20]),
+        height,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -208,7 +238,10 @@ mod tests {
         let tinycash = BoxService::new(TinyCashWriteService::new(state_service, verifier_service));
 
         tinycash
-            .oneshot(Request::Mint { amount: Amount::zero(), to: transparent::Address::from_script_hash(network, [0;20]) })
+            .oneshot(Request::Mint {
+                amount: Amount::try_from(100).unwrap(),
+                to: transparent::Address::from_script_hash(network, [0; 20]),
+            })
             .await
             .expect("unexpected error response");
     }
