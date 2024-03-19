6fd7121bef1bdf524fff2ae53118b2af37dbfc17
Willem Olding <willemolding@gmail.com>
2024-03-19 18:25:54 +1100

    adds stubbed implementation of service

--- tiny-cash/src/write.rs
+++ tiny-cash/src/write.rs
@@ -1,9 +1,16 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use futures_util::future::FutureExt;
+
 use tower::Service;
 use zebra_chain::amount::{Amount, NonNegative};
-use zebra_chain::transparent;
 use zebra_chain::block;
 use zebra_chain::transaction::Transaction;
-
+use zebra_chain::transparent;
+use zebra_consensus::transaction as tx;
 
 pub struct TinyCashWriteService<S, V> {
     state_service: S,
@@ -38,4 +45,46 @@ pub struct Response {
     block_hash: block::Hash,
     /// The amount of coins that were burned by the transaction (if any) by transferring to the Mt Doom address (0x000...000)
     burned: Amount<NonNegative>,
+}
+
+impl<S, V> tower::Service<Request> for TinyCashWriteService<S, V>
+where
+    S: Service<
+            zebra_state::Request,
+            Response = zebra_state::Response,
+            Error = zebra_state::BoxError,
+        >
+        + Send
+        + Clone
+        + 'static
+        + Clone,
+    S::Future: Send + 'static,
+    V: Service<
+            tx::Request,
+            Response = tx::Response,
+            Error = zebra_consensus::error::TransactionError,
+        >
+        + Send
+        + Clone
+        + 'static
+        + Clone,
+    V::Future: Send + 'static,
+{
+    type Response = Response;
+    type Error = zebra_consensus::BoxError;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.state_service.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request) -> Self::Future {
+        async move {
+            Ok(Response {
+                block_hash: block::Hash([0; 32]),
+                burned: Amount::zero(),
+            })
+        }.boxed()
+    }
 }
