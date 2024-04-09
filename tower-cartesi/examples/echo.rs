6560be976f14066c6a2b1b6467fa237080527367
Willem Olding <willemolding@gmail.com>
2024-04-09 20:03:22 +1000

    global format

--- tower-cartesi/examples/echo.rs
+++ tower-cartesi/examples/echo.rs
@@ -1,12 +1,12 @@
-use std::env;
-use std::future::Future;
-use std::error::Error;
-use std::pin::Pin;
-use std::task::{Poll, Context};
 use futures_util::FutureExt;
+use std::env;
+use std::error::Error;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
-use tower_service::Service;
 use tower_cartesi::{listen_http, Request, Response};
+use tower_service::Service;
 
 type BoxError = Box<dyn Error + Send + Sync>;
 
@@ -35,24 +35,22 @@ impl Service<Request> for EchoApp {
     type Error = Box<dyn Error + Send + Sync>;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-    fn poll_ready(
-        &mut self,
-        _cx: &mut Context<'_>,
-    ) -> Poll<Result<(), Self::Error>> {
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         Poll::Ready(Ok(()))
     }
 
     fn call(&mut self, req: Request) -> Self::Future {
         match req {
             Request::AdvanceState { metadata, payload } => {
-                println!("Received advance state request {:?} \npayload {:?}:", metadata, payload);
+                println!(
+                    "Received advance state request {:?} \npayload {:?}:",
+                    metadata, payload
+                );
             }
             Request::InspectState { payload } => {
                 println!("Received inspect state request {:?}", payload);
             }
         }
-        async {
-            Ok(tower_cartesi::Response::empty_accept())
-        }.boxed()
+        async { Ok(tower_cartesi::Response::empty_accept()) }.boxed()
     }
 }
