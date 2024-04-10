4a3cdfd67817976efb2b69f94f85f75691a8a858
Willem Olding <willemolding@gmail.com>
2024-04-10 14:13:40 +1000

    Update echo.rs

--- tower-cartesi/examples/echo.rs
+++ tower-cartesi/examples/echo.rs
@@ -6,19 +6,10 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use tower_cartesi::{listen_http, Request, Response};
-use tower_service::Service;
-
-type BoxError = Box<dyn Error + Send + Sync>;
+use tower_service::{BoxError, Service};
 
 #[tokio::main]
 async fn main() -> Result<(), BoxError> {
-    let subscriber = tracing_subscriber::fmt()
-        .without_time()
-        .with_max_level(tracing::Level::INFO)
-        .compact()
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)?;
-
     let server_addr = env::var("ROLLUP_HTTP_SERVER_URL")?;
 
     let mut app = EchoApp {};
@@ -32,7 +23,7 @@ struct EchoApp;
 
 impl Service<Request> for EchoApp {
     type Response = Response;
-    type Error = Box<dyn Error + Send + Sync>;
+    type Error = BoxError;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
